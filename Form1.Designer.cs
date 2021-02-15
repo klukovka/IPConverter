6aaf303075c5e418a61421f58d350fe3a662148c
klukovka <58118015+klukovka@users.noreply.github.com>
2021-02-15 16:51:10 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace IPconverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.MOct1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.MOct2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.MOct3 = new System.Windows.Forms.TextBox();
+            this.MOct4 = new System.Windows.Forms.TextBox();
+            this.IPOct4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.IPOct3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.IPOct2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.IPOct1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.Result = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.pref = new System.Windows.Forms.TextBox();
+            this.Number = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Constantia", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(48, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Маска подсети";
+            // 
+            // MOct1
+            // 
+            this.MOct1.Location = new System.Drawing.Point(54, 152);
+            this.MOct1.Name = "MOct1";
+            this.MOct1.Size = new System.Drawing.Size(120, 26);
+            this.MOct1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Constantia", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(175, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 35);
+            this.label2.TabIndex = 2;
+            this.label2.Text = ".";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Constantia", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(325, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 35);
+            this.label3.TabIndex = 4;
+            this.label3.Text = ".";
+            // 
+            // MOct2
+            // 
+            this.MOct2.Location = new System.Drawing.Point(204, 152);
+            this.MOct2.Name = "MOct2";
+            this.MOct2.Size = new System.Drawing.Size(120, 26);
+            this.MOct2.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Constantia", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(475, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 35);
+            this.label4.TabIndex = 6;
+            this.label4.Text = ".";
+            // 
+            // MOct3
+            // 
+            this.MOct3.Location = new System.Drawing.Point(354, 152);
+            this.MOct3.Name = "MOct3";
+            this.MOct3.Size = new System.Drawing.Size(120, 26);
+            this.MOct3.TabIndex = 5;
+            // 
+            // MOct4
+            // 
+            this.MOct4.Location = new System.Drawing.Point(504, 152);
+            this.MOct4.Name = "MOct4";
+            this.MOct4.Size = new System.Drawing.Size(120, 26);
+            this.MOct4.TabIndex = 7;
+            // 
+            // IPOct4
+            // 
+            this.IPOct4.Location = new System.Drawing.Point(504, 261);
+            this.IPOct4.Name = "IPOct4";
+            this.IPOct4.Size = new System.Drawing.Size(120, 26);
+            this.IPOct4.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Constantia", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(475, 252);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 35);
+            this.label5.TabIndex = 14;
+            this.label5.Text = ".";
+            // 
+            // IPOct3
+            // 
+            this.IPOct3.Location = new System.Drawing.Point(354, 261);
+            this.IPOct3.Name = "IPOct3";
+            this.IPOct3.Size = new System.Drawing.Size(120, 26);
+            this.IPOct3.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Constantia", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(325, 252);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 35);
+            this.label6.TabIndex = 12;
+            this.label6.Text = ".";
+            // 
+            // IPOct2
+            // 
+            this.IPOct2.Location = new System.Drawing.Point(204, 261);
+            this.IPOct2.Name = "IPOct2";
+            this.IPOct2.Size = new System.Drawing.Size(120, 26);
+            this.IPOct2.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Constantia", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(175, 252);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 35);
+            this.label7.TabIndex = 10;
+            this.label7.Text = ".";
+            // 
+            // IPOct1
+            // 
+            this.IPOct1.Location = new System.Drawing.Point(54, 261);
+            this.IPOct1.Name = "IPOct1";
+            this.IPOct1.Size = new System.Drawing.Size(120, 26);
+            this.IPOct1.TabIndex = 9;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Constantia", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(48, 214);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(194, 35);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "IP-адрес узла";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Constantia", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(48, 394);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(247, 35);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "IP-адрес подсети";
+            // 
+            // Result
+            // 
+            this.Result.AutoSize = true;
+            this.Result.Font = new System.Drawing.Font("Constantia", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Result.Location = new System.Drawing.Point(48, 445);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(23, 35);
+            this.Result.TabIndex = 17;
+            this.Result.Text = ".";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Location = new System.Drawing.Point(54, 322);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(570, 48);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Получить!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Constantia", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(48, 24);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(229, 35);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Префикс маски";
+            // 
+            // pref
+            // 
+            this.pref.Location = new System.Drawing.Point(54, 76);
+            this.pref.Name = "pref";
+            this.pref.Size = new System.Drawing.Size(120, 26);
+            this.pref.TabIndex = 20;
+            this.pref.TextChanged += new System.EventHandler(this.pref_TextChanged);
+            // 
+            // Number
+            // 
+            this.Number.AutoSize = true;
+            this.Number.Font = new System.Drawing.Font("Constantia", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Number.Location = new System.Drawing.Point(48, 546);
+            this.Number.Name = "Number";
+            this.Number.Size = new System.Drawing.Size(23, 35);
+            this.Number.TabIndex = 22;
+            this.Number.Text = ".";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Constantia", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12.Location = new System.Drawing.Point(48, 495);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(169, 35);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "Номер узла";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(656, 621);
+            this.Controls.Add(this.Number);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.pref);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.IPOct4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.IPOct3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.IPOct2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.IPOct1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.MOct4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.MOct3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.MOct2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.MOct1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "IP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox MOct1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox MOct2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox MOct3;
+        private System.Windows.Forms.TextBox MOct4;
+        private System.Windows.Forms.TextBox IPOct4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox IPOct3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox IPOct2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox IPOct1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label Result;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox pref;
+        private System.Windows.Forms.Label Number;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IPconverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] mask = new string[] { ConvertDecToSmth(Convert.ToInt32(MOct1.Text), 2), ConvertDecToSmth(Convert.ToInt32(MOct2.Text), 2),
+              ConvertDecToSmth(Convert.ToInt32(MOct3.Text), 2),ConvertDecToSmth(Convert.ToInt32(MOct4.Text), 2)};
+            string[] ip = new string[] { ConvertDecToSmth(Convert.ToInt32(IPOct1.Text), 2), ConvertDecToSmth(Convert.ToInt32(IPOct2.Text), 2),
+              ConvertDecToSmth(Convert.ToInt32(IPOct3.Text), 2),ConvertDecToSmth(Convert.ToInt32(IPOct4.Text), 2)};
+            for (int i = 0; i < 4; i++)
+            {
+                mask[i] = mask[i].PadLeft(8, '0');
+                ip[i] = ip[i].PadLeft(8, '0');
+            }
+
+            string[] res = new string[4];
+            int[] num = new int[] {Convert.ToInt32(IPOct1.Text),
+            Convert.ToInt32(IPOct2.Text),
+            Convert.ToInt32(IPOct3.Text),
+            Convert.ToInt32(IPOct4.Text)};
+
+            string number = "";
+            string result = "";
+            for (int i = 0; i < 4; i++)
+            {
+             
+                res[i] = "";
+                for (int j = 0; j < 8; j++)
+                    res[i] += (mask[i][j] == '1') && (ip[i][j] == '1') ? "1" : "0";
+                result += ConvertToDec(res[i], 2) + ".";
+                num[i] -= Convert.ToInt32(ConvertToDec(res[i], 2));
+             number += num[i].ToString() + ".";
+            }
+            result = result.TrimEnd('.');
+            number = number.TrimEnd('.');
+            Result.Text = result;
+            Number.Text = number;
+
+       
+        }
+        private string ConvertToDec(string num, int based)
+        {
+            num = num.ToUpper();
+            char[] val = num.ToCharArray();
+            Array.Reverse(val);
+            double res = 0;
+            for (int i = 0; i < val.Length; i++)
+            {
+                int temp;
+                switch (val[i])
+                {
+                    case 'A':
+                        temp = 10;
+                        break;
+                    case 'B':
+                        temp = 11;
+                        break;
+                    case 'C':
+                        temp = 12;
+                        break;
+                    case 'D':
+                        temp = 13;
+                        break;
+                    case 'E':
+                        temp = 14;
+                        break;
+                    case 'F':
+                        temp = 15;
+                        break;
+                    default:
+                        temp = Convert.ToInt32(val[i].ToString());
+                        break;
+                }
+                res += temp * Math.Pow(based, i);
+
+            }
+            return res.ToString();
+        }
+        private string ConvertDecToSmth(int num, int a)
+        {
+            string res = "";
+            List<int> list = new List<int>();
+            while (num >= a)
+            {
+                list.Add(num % a);
+                num = Convert.ToInt32(Math.Floor((decimal)num / a));
+            }
+            list.Add(num);
+
+            list.Reverse();
+            foreach (int item in list)
+            {
+                switch (item)
+                {
+                    case 10:
+                        res += "A";
+                        break;
+                    case 11:
+                        res += "B";
+                        break;
+                    case 12:
+                        res += "C";
+                        break;
+                    case 13:
+                        res += "D";
+                        break;
+                    case 14:
+                        res += "E";
+                        break;
+                    case 15:
+                        res += "F";
+                        break;
+                    default:
+                        res += item.ToString();
+                        break;
+                }
+
+
+            }
+            return res;
+        }
+
+        private void pref_TextChanged(object sender, EventArgs e)
+        {
+            if (pref.Text != "")
+            {
+                string nums = "";
+                for (int i = 0; i < Convert.ToInt32(pref.Text); i++)
+                    nums += "1";
+                nums = nums.PadRight(32, '0');
+                MOct1.Text = ConvertToDec(nums.Substring(0, 8), 2);
+                MOct2.Text = ConvertToDec(nums.Substring(8, 8), 2);
+                MOct3.Text = ConvertToDec(nums.Substring(16, 8), 2);
+                MOct4.Text = ConvertToDec(nums.Substring(24, 8), 2);
+            }
+        }
+    }
+}
